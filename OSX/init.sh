238791b810ead5a3369a63f86d5d9d02b553d008
iyahoo <s1200191@gmail.com>
2016-01-19 15:37:43 +0900

    [Update]

--- OSX/init.sh
+++ OSX/init.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+# You have to install xcode from App store
 sudo xcodebuild -license
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install python
+brew install ansible
+mkdir .macbook-provisioning
+cd .macbook-provisioning/
+echo 'localhost' > hosts
+
